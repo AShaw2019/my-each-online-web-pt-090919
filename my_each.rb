f56b8bedf82aca5c3a32cb2a1f711f82766e4f04
Ashley Twomey <aprier091012@gmail.com>
2019-09-19 01:09:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
- def my_each(array)
+ def hello(array)
   i = 0
-  collection = ["arel", "jon", "logan", "spencer"]
+  collection = []
   while i < array.length
-   collection << yield(array[i])
-    i += 1 
+    collection << yield(array[i])
+    i += 1
   end
   collection
 end
