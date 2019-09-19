8f4fc35f35c738aa30806090556456b9c4e809ab
Ashley Twomey <aprier091012@gmail.com>
2019-09-19 01:08:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@
   i = 0
   collection = ["arel", "jon", "logan", "spencer"]
   while i < array.length
-   collection << yield(array[i])
+   collection << yield(array["arel", "jon", "logan", "spencer"])
     i += 1 
   end
   collection
